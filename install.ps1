0fa60b79ba47fba03c19676042aa5851547aad99
Chris Galvan <chrisgalvanr@gmail.com>
2018-08-13 14:58:21 -0500

    fix: check for text before adding it (avoid dups)

--- install.ps1
+++ install.ps1
@@ -1,5 +1,11 @@
 $profileFiles = $PSScriptRoot
+$profilePath = "$profileFiles\profile.ps1"
 
-Add-Content $PROFILE ". `"$profileFiles\profile.ps1`""
+$result = Get-Content $PROFILE | Select-String $profilePath.replace("\", "\\")
 
-. $PROFILE
+if ($result -eq $null) {
+  Add-Content $PROFILE ". `"$profilePath`""
+}
+
+
+#. $PROFILE
